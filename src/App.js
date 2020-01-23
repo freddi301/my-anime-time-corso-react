dfd79e4ba8c02fdf3b23315a3f243a119e4df64f
Frederik Batuna <frederik.batuna@smc.it>
2020-01-23 15:40:09 +0100

    add: styled components to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
   return (
@@ -37,33 +38,55 @@ export default App;
 // navbar
 function Navbar() {
   return (
-    <div>
-      <ol>
-        <li>
-          <Link to="/popolari">popolari</Link>
-        </li>
-        <li>
-          <Link to="/scopri">scopri</Link>
-        </li>
-        <li>
-          <Link to="/ricerca">ricerca</Link>
-        </li>
-        <li>
-          <Link to="/continua">continua</Link>
-        </li>
-        <li>
-          <Link to="/completati">completati</Link>
-        </li>
-        <li>
-          <Link to="/belli">belli</Link>
-        </li>
-        <li>
-          <Link to="/brutti">brutti</Link>
-        </li>
-      </ol>
-    </div>
+    <StyledNavbarContainer>
+      <StyledNavbarItem>
+        <Link to="/popolari">popolari</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/scopri">scopri</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/ricerca">ricerca</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/continua">continua</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/completati">completati</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/belli">belli</Link>
+      </StyledNavbarItem>
+      <StyledNavbarItem>
+        <Link to="/brutti">brutti</Link>
+      </StyledNavbarItem>
+    </StyledNavbarContainer>
   );
 }
+
+const StyledNavbarContainer = styled.div`
+  display: flex;
+  box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.75);
+`;
+
+const StyledNavbarItem = styled.div`
+  flex-grow: 1;
+  min-height: 1cm;
+  min-width: 1cm;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
+
+// function StyledNavbarContainerCompiled(props) {
+//   return (
+//     <div {...props} style={{ display: "flex" }}>
+//       {props.children}
+//     </div>
+//   );
+// }
 
 // - anime piu popolari
 //   - lista di anime (non filtrata, ordinata in base popolarità piu alta)
