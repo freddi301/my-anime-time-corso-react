80c10f4b896c9c73a5773aec6edd07239fc0dce0
Frederik Batuna <frederik.batuna@smc.it>
2020-01-23 13:26:53 +0100

    add: navbar mock

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
 export default App;
 
 // navbar
+function Navbar(){
+  return <div>
+    <ol>
+      <li>
+        popolari
+      </li>
+      <li>
+        scopri
+      </li>
+      <li>
+        ricerca
+      </li>
+      <li>
+        continua
+      </li>
+      <li>
+        completati
+      </li>
+      <li>
+        belli
+      </li>
+      <li>
+        brutti
+      </li>
+    </ol>
+  </div>
+}
 
 // - anime piu popolari
 //   - lista di anime (non filtrata, ordinata in base popolarità piu alta)
@@ -34,13 +44,13 @@ export default App;
 //   - lista di anime (filtrata per quelli non seguiti, ordinata in base popolarità piu alta)
 
 // - anime piu belli che ho visto
-//   - lista di anime filtro=personale ordinamento=decrescente
+//   - lista di anime filtro=personale ordinamento=decrescente (valutazione personale)
 
 // - anime continua a guardare
 //   - lista di anime filtro=seguito ordinamento=decrescente (valutazione globale)
 
 // - anime completati
-//   - lista di anime filtro=progressione==max ordinamento=decrescente (valutazione personale)
+//   - lista di anime filtro=progressione==max ordinamento=decrescente 
 
 // - anime più brutti
 //   - lista di anime filtro=pregressione==max ordinamento crescente (sulla valutazione personale)
